7b02fd6822d4df8b09ab1d207b05b906c8252dfa
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-02-02 15:50:58 +0100

    Tested with modified workflow
    * works with btw21 data, but code not reviewed and properly tested yet

--- test.jl
+++ test.jl
@@ -1,1 +1,9 @@
+using Pkg
+Pkg.activate(".")
+
 using Negotiations
+
+params = parameter_set_from_config("config.yaml")
+db = load_database("db.sqlite3")
+model = setup_model(params, db)
+sequences, data = simulate(model, 1)
