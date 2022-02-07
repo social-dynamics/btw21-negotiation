f0c76aa35f40f18eb3f0e8623ad40a3ca9db4980
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-02-07 19:59:36 +0100

    Changed shebang in get_data

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 wget https://www.bpb.de/system/files/datei/Wahl-O-Mat%20Bundestag%202021_Datensatz_v1.02.zip -P data
 7z x -odata data/'Wahl-O-Mat Bundestag 2021_Datensatz_v1.02.zip'
 rm data/'Wahl-O-Mat Bundestag 2021_Datensatz_v1.02.zip'
