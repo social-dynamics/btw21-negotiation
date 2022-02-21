034cd1cf4b2f8423195485da29fa3b9939eebe60
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-02-21 14:34:06 +0100

    Added run with different rule

--- run_model.jl
+++ run_model.jl
@@ -12,5 +12,6 @@ using Negotiations
 params = parameter_set_from_config("config.yaml")
 db = load_database("db.sqlite")
 model = setup_model(params, db)
-rule = BoundedConfidence(bc = 1.0, inertia = 10.0)
+# rule = BoundedConfidence(bc = 1.0, inertia = 10.0)
+rule = ContinuousHomophily(inertia = 20.0)
 @time simulate(model, rule, 3, db, batchname = "test")
