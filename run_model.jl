dd6828416598e7bed620c8865328ab4e6ec5616b
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-02-21 13:48:49 +0100

    Updated model run to newest state of Negotiations

--- run_model.jl
+++ run_model.jl
@@ -12,4 +12,5 @@ using Negotiations
 params = parameter_set_from_config("config.yaml")
 db = load_database("db.sqlite")
 model = setup_model(params, db)
-@time simulate(model, 3, db, batchname = "test")
+rule = BoundedConfidence(bc = 1.0, inertia = 10.0)
+@time simulate(model, rule, 3, db, batchname = "test")
